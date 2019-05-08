be4c981ff7ef641ebc96cf2166d230fa9ffa83d0
Smita Deshpande <smipra11@gmail.com>
2019-05-08 10:24:29 -0400

    files added

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Hot Spicy Jack');
+INSERT INTO burgers(burger_name) VALUES ('Ultimate Chicken');
+INSERT INTO burgers(burger_name, devoured) VALUES ('Black Bean Avacado', true);
+INSERT INTO burgers(burger_name, devoured) VALUES ('Big Beef Loaded', true);
